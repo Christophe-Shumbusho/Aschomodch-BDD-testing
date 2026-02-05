9582eeb55cd661200eada80ad8732e4914841e6c
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2026-02-05 11:50:38 +0200

    set headless mode

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -3,8 +3,10 @@ package Hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
@@ -16,8 +18,13 @@ public class Hooks {
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // run without GUI
+        options.addArguments("--no-sandbox"); // required in CI
+        options.addArguments("--disable-dev-shm-usage"); // prevent crashes in limited /dev/shm
+        options.addArguments("--remote-allow-origins=*"); // fixes some CI/CD origin issues
+        driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1920,1080)); // instead of maximize }
     }
     @After
     public void tearDown(){
